ed5eeacc62e37690229044feeb8872bdb9625fa2
R0CK3TDEV <ezequielzapata2023@proton.me>
2026-06-22 16:26:31 -0300

    tests pasaron

--- src/errors.js
+++ src/errors.js
@@ -1,29 +1,47 @@
 class BadRequestError extends Error {
-  constructor(message) {
-    super(message);
-    this.name = "BadRequestError";
-    this.statusCode = 400;
-  }
+    constructor(message) {
+        super(message);
+        this.name = "BadRequestError";
+        this.statusCode = 400;
+    }
 }
 
 class TargetAnalysisError extends Error {
-  constructor(message = "Target could not be analyzed") {
-    super(message);
-    this.name = "TargetAnalysisError";
-    this.statusCode = 422;
-  }
+    constructor(message = "Target could not be analyzed") {
+        super(message);
+        this.name = "TargetAnalysisError";
+        this.statusCode = 422;
+    }
 }
 
 class TargetTimeoutError extends Error {
-  constructor(message = "Timeout while loading target page") {
-    super(message);
-    this.name = "TargetTimeoutError";
-    this.statusCode = 504;
-  }
+    constructor(message = "Timeout while loading target page") {
+        super(message);
+        this.name = "TargetTimeoutError";
+        this.statusCode = 504;
+    }
+}
+
+class TemporarilyUnavailableError extends Error {
+    constructor(message = "The server is temporarily unavailable") {
+        super(message);
+        this.name = "TemporarilyUnavailableError";
+        this.statusCode = 503;
+    }
+}
+
+class InternalServerError extends Error {
+    constructor(message = "The server was unable to complete your request") {
+        super(message);
+        this.name = "InternalServerError";
+        this.statusCode = 500;
+    }
 }
 
 module.exports = {
-  BadRequestError,
-  TargetAnalysisError,
-  TargetTimeoutError
+    BadRequestError,
+    TargetAnalysisError,
+    TargetTimeoutError,
+    TemporarilyUnavailableError,
+    InternalServerError,
 };
--- src/routes/analyze.routes.js
+++ src/routes/analyze.routes.js
@@ -2,32 +2,36 @@ const express = require("express");
 const { validateAnalyzeRequest } = require("../middlewares");
 
 module.exports = function analyzeRoutes({
-  analyzerService,
-  loggingService,
-  config
+    analyzerService,
+    loggingService,
+    config,
 }) {
-  const router = express.Router();
+    const router = express.Router();
 
-  router.post("/analyze", validateAnalyzeRequest(config), async (req, res, next) => {
-    try {
-      const { url, options } = req.analysisRequest;
+    router.post(
+        "/analyze",
+        validateAnalyzeRequest(config),
+        async (req, res, next) => {
+            try {
+                const { url, options } = req.analysisRequest;
 
-      await loggingService.logIncomingAnalyzeRequest({ url });
-      await loggingService.logRobotRequest({ url });
+                await loggingService.logIncomingAnalyzeRequest({ url });
+                await loggingService.logRobotRequest({ url });
 
-      const response = await analyzerService.analyze(url, options);
+                const response = await analyzerService.analyze(url, options);
 
-      await loggingService.logRobotResponse({
-        url,
-        statusCode: 200
-      });
+                await loggingService.logRobotResponse({
+                    url,
+                    statusCode: 200,
+                });
 
-      return res.status(200).json(response);
-    } catch (error) {
-      await loggingService.logError(error);
-      return next(error);
-    }
-  });
+                return res.status(200).json(response);
+            } catch (error) {
+                await loggingService.logError(error);
+                return next(error);
+            }
+        },
+    );
 
-  return router;
+    return router;
 };
